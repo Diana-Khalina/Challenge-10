7ae2e65fd4aefe6c1abde7bbb89a6be9fc089cb5
Diana-Khalina <diaaana@i.ua>
2025-02-11 17:22:56 -0500

    working on index.ts, connectiont.ts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,42 +1,46 @@
-INSERT INTO department (name) VALUES
-  ('Operations'),
-  ('Human Resources'),
-  ('Technology'),
-  ('Logistics'),
-  ('Finance'),
-  ('Marketing'),
-  ('Customer Service');
+INSERT INTO department (name) 
+VALUES 
+('Store Operations'), 
+('Merchandising'), 
+('Supply Chain & Logistics'), 
+('Finance & Accounting'), 
+('Human Resources'), 
+('Marketing'), 
+('Technology'), 
+('Customer Service');
 
-INSERT INTO role (title, salary, department_id) VALUES
-  ('Operations Manager', 95000, (SELECT id FROM department WHERE name = 'Operations')),
-  ('Operations Coordinator', 60000, (SELECT id FROM department WHERE name = 'Operations')),
-  ('HR Specialist', 70000, (SELECT id FROM department WHERE name = 'Human Resources')),
-  ('HR Manager', 100000, (SELECT id FROM department WHERE name = 'Human Resources')),
-  ('Technical Specialist', 85000, (SELECT id FROM department WHERE name = 'Technology')),
-  ('Chief Technology Officer', 130000, (SELECT id FROM department WHERE name = 'Technology')),
-  ('Logistics Coordinator', 55000, (SELECT id FROM department WHERE name = 'Logistics')),
-  ('Logistics Manager', 90000, (SELECT id FROM department WHERE name = 'Logistics')),
-  ('Accountant', 85000, (SELECT id FROM department WHERE name = 'Finance')),
-  ('Finance Manager', 120000, (SELECT id FROM department WHERE name = 'Finance')),
-  ('Marketing Specialist', 75000, (SELECT id FROM department WHERE name = 'Marketing')),
-  ('Marketing Director', 110000, (SELECT id FROM department WHERE name = 'Marketing')),
-  ('Customer Support Representative', 50000, (SELECT id FROM department WHERE name = 'Customer Service')),
-  ('Customer Service Manager', 80000, (SELECT id FROM department WHERE name = 'Customer Service'));
+INSERT INTO role (title, salary, department_id)
+VALUES 
+('Store Manager', 100000, 1),
+('Assistant Store Manager', 60000, 1),
+('Cashier', 35000, 1),
+('Sales Associate', 40000, 2),
+('Visual Merchandiser', 50000, 2),
+('Logistics Coordinator', 55000, 3),
+('Warehouse Supervisor', 60000, 3),
+('Finance Analyst', 75000, 4),
+('HR Coordinator', 50000, 5),
+('Recruiter', 60000, 5),
+('Marketing Specialist', 65000, 6),
+('Social Media Manager', 70000, 6),
+('Software Engineer', 90000, 7),
+('IT Support Specialist', 65000, 7),
+('Customer Service Representat', 40000, 8);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-  ('Alex', 'Ivanenko', (SELECT id FROM role WHERE title = 'Operations Manager'), NULL),
-  ('Tatyana', 'Petrenko', (SELECT id FROM role WHERE title = 'HR Manager'), NULL),
-  ('Maxim', 'Kovalenko', (SELECT id FROM role WHERE title = 'Chief Technology Officer'), NULL),
-  ('Irina', 'Honcharenko', (SELECT id FROM role WHERE title = 'Logistics Manager'), NULL),
-  ('Oleg', 'Danyliuk', (SELECT id FROM role WHERE title = 'Finance Manager'), NULL),
-  ('Anastasia', 'Shevchenko', (SELECT id FROM role WHERE title = 'Marketing Director'), NULL),
-  ('Vladimir', 'Melnyk', (SELECT id FROM role WHERE title = 'Customer Service Manager'), NULL);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-  ('Maria', 'Sydorenko', (SELECT id FROM role WHERE title = 'Operations Coordinator'), (SELECT id FROM employee WHERE first_name = 'Alex' AND last_name = 'Ivanenko')),
-  ('Eugene', 'Lysenko', (SELECT id FROM role WHERE title = 'HR Specialist'), (SELECT id FROM employee WHERE first_name = 'Tatyana' AND last_name = 'Petrenko')),
-  ('Artem', 'Romanenko', (SELECT id FROM role WHERE title = 'Technical Specialist'), (SELECT id FROM employee WHERE first_name = 'Maxim' AND last_name = 'Kovalenko')),
-  ('Alexander', 'Tkachenko', (SELECT id FROM role WHERE title = 'Logistics Coordinator'), (SELECT id FROM employee WHERE first_name = 'Irina' AND last_name = 'Honcharenko')),
-  ('Dmitry', 'Zakharchenko', (SELECT id FROM role WHERE title = 'Accountant'), (SELECT id FROM employee WHERE first_name = 'Oleg' AND last_name = 'Danyliuk')),
-  ('Katherine', 'Bondarenko', (SELECT id FROM role WHERE title = 'Marketing Specialist'), (SELECT id FROM employee WHERE first_name = 'Anastasia' AND last_name = 'Shevchenko')),
-  ('Julia', 'Moroz', (SELECT id FROM role WHERE title = 'Customer Support Representative'), (SELECT id FROM employee WHERE first_name = 'Vladimir' AND last_name = 'Melnyk'));
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES 
+('Andriy', 'Shevchenko', 1, NULL), 
+('Olena', 'Zelenska', 2, 1),  
+('Dmytro', 'Koval', 3, 2), 
+('Natalia', 'Bondarenko', 4, NULL), 
+('Ihor', 'Melnyk', 5, 4),  
+('Kateryna', 'Tkachenko', 6, NULL),  
+('Volodymyr', 'Lysenko', 7, 6),  
+('Svitlana', 'Hrytsenko', 8, NULL),  
+('Yaroslav', 'Pavlenko', 9, NULL),  
+('Oksana', 'Romanenko', 10, 9),  
+('Bohdan', 'Soroka', 11, NULL),  
+('Larysa', 'Fedorenko', 12, 11),  
+('Oleksandr', 'Mazur', 13, NULL),  
+('Sofia', 'Rudenko', 14, 13),  
+('Petro', 'Chornyi', 15, NULL); 
